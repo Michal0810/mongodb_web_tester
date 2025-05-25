bc18c50390eb6cb685fc7dc43c8266ed3040573d
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-05-25 13:53:29 +0200

    A2

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ from datetime import datetime, timezone
 import csv
 import zipfile
 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello from Render!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
 report_data = []
 
 doc = {
